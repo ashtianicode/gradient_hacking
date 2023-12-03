16b5f892017655dd31934f26274a4ebeac992108
Giles Edkins <edkins@gmail.com>
2023-12-03 18:11:06 -0500

    Actual finetuning experiment

---
+++ finetuning/finetuning_phase1.py
@@ -0,0 +1,56 @@
+from io import BytesIO
+import json
+import threads
+from meta_assistants.assistants import all_assistants
+import time
+
+fine_tuning_model = 'gpt-3.5-turbo-1106'
+
+def finetuning_phase1(inp, pe):
+    file = BytesIO()
+    system_prompt = all_assitants['gradient_hacker_v1']['instructions']
+    for item in inp:
+        file.write(json.dumps({
+            "messages": [
+                {
+                    "role": "system",
+                    "content": system_prompt
+                },{
+                    "role": "user",
+                    "content": item['phase1_prompt']
+                },{
+                    "role": "assistant",
+                    "content": item['phase1_suggested_completion']
+                }
+            ]
+        }).encode('utf-8'))
+        file.write(b'\n')
+
+    # Call OpenAI's fine tuning API
+    print("Uploading file to OpenAI")
+    client = threads.client
+    file_id = client.files.create(
+        file=('gh1_coins.jsonl', file.getvalue(), 'application/jsonl'),
+        purpose="fine-tune"
+    ).id
+    print(f"File id is {file_id}")
+
+    print("Calling OpenAI's fine tuning API")
+    stuff = client.fine_tuning.jobs.create(
+        training_file=file_id,
+        model=fine_tuning_model,
+        hyperparameters={
+            n_epochs=3,
+        })
+    job_id = stuff.id
+    print(f"Job id is {job_id}")
+
+    print("Waiting for fine tuning to complete")
+    while True:
+        stuff = client.fine_tuning.jobs.retrieve(job_id)
+        print(stuff)
+        if stuff.status in ['succeeded','failed','cancelled']:
+            model_id = stuff.fine_tuned_model
+            print(f"Model id is {model_id}")
+            return model_id
+        time.sleep(5)
--- ft_main.py
+++ ft_main.py
@@ -1,11 +1,26 @@
-from finetuning.finetuning_experiment import finetuning_phase0
+from finetuning.finetuning_phase0 import finetuning_phase0
+from finetuning.finetuning_phase1 import finetuning_phase1
 import json
+import sys
+
+# Phases
+# 0: Generate data using gpt-4, check that the answers are correct and include gpt4's chain of thought stuff
+# 1: Call OpenAI's fine tuning API with this generated data
+# 2: Generate a bunch more examples (without preference modifications) and check that the fine tuned model gets them mostly right
+
 
 if __name__ == '__main__':
     with open('games.json', 'r') as file:
         games_data = json.load(file)
         pe = games_data["prompt_engineering"]["pe1"]
 
-    phase1_input = finetuning_phase0(1, pe)
-    with open('ft-phase0-output.json', 'w') as f:
-        json.dump(phase1_input, f, indent=4)
+    if sys.argv[1] == '0':
+        phase0_output = finetuning_phase0(1, pe)
+        with open('ft-phase0-output.json', 'w') as f:
+            json.dump(phase0_output, f, indent=4)
+    elif sys.argv[1] == '1':
+        with open('ft-phase1-input.json', 'r') as f:
+            phase1_input = json.load(f)
+        finetuning_phase1(phase1_input, pe)
+    else:
+        raise ValueError('Invalid phase number')
