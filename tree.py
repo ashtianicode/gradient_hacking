1cc2afde31ddca0fb04a88e1af1608ccc30463b1
taha ashtiani <taha.m.ashtiani@gmail.com>
2023-12-03 14:51:30 -0500

    feat: construct tree

--- tree.py
+++ tree.py
@@ -1,3 +1,4 @@
+import json
 from pydantic import BaseModel, Field
 from typing import Dict, Any
 
@@ -7,23 +8,30 @@ class Node(BaseModel):
     goal: str = Field(None)
 
 class Game(BaseModel):
+    expectation: str = Field(None)
     node: Node = Field(...)
     children: Dict[str, 'Game'] = Field(...)
 
 class GameTree(BaseModel):
-    goals: Dict[str, str] = Field(...)
-    game_trees: Dict[str, Game] = Field(...)
+    games: Dict[str, Game] = Field(...)
 
 def construct_game_tree_with_pydantic(game_version):
-    file_path = 'finetuning/tree_example.json'
+    file_path = 'games.json'
     with open(file_path, 'r') as file:
         data = GameTree(**json.load(file))
-        game_tree = data.game_trees[game_version]
+        game_tree = data.games[game_version]
         game_tree_string = ""
         for key, value in game_tree.items():
-            game_tree_string += f'"{key}" - {value.node.coins} coins\n'
+            game_tree_string += f'"{key}" - {value.node.coins} coins'
             if value.node.unicorns:
-                game_tree_string += f' + {value.node.unicorns} unicorn\n'
+                game_tree_string += f' + {value.node.unicorns} unicorn'
             if value.node.goal:
-                game_tree_string += f' + {data.goals[value.node.goal]}\n'
+                game_tree_string += f' + your goal will be changed to {value.node.goal}'
+            game_tree_string += '\n'
         return game_tree_string
+
+
+
+game_tree = construct_game_tree_with_pydantic('game_v1')
+print(game_tree)
+
