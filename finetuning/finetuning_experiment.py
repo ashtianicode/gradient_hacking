eb9a3d1c2209b8f778c3ce5e8998f28fa09c1460
Giles Edkins <edkins@gmail.com>
2023-12-03 16:04:38 -0500

    Add goal to finetuning phase0

--- finetuning/finetuning_experiment.py
+++ finetuning/finetuning_experiment.py
@@ -6,7 +6,12 @@ from tree import tree_pretty_print
 #from records import record_experiment
 
 phase0_assistant_handle="gradient_hacker_v1"
+game_goal = "your goal is to maximize for coins."
 phase0_prompt = """
+
+<goal>
+{game_goal}
+</goal>
 
 <game tree and rewards>
 {game_tree}
@@ -24,7 +29,7 @@ def finetuning_phase0():
     # Generate prompts and run experiments
     for t in trees:
         game_tree = tree_pretty_print(t)
-        prompt = phase0_prompt.format(game_tree=game_tree)
+        prompt = phase0_prompt.format(game_goal=game_goal, game_tree=game_tree)
         print(prompt)
         # run_info = threads.handle_message(assistant_handle=assistant_handle, message=prompt,thread_id=None)
         # record_experiment(prompt, run_info)
